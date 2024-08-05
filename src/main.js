74cb63ceca020cc5432d2eef8b2c0af046ded298
JongHwanJeong <96283391+babyslayerr@users.noreply.github.com>
2024-08-06 07:58:26 +0900

    axios 라이브러리 설치, main.js에 설명 주석 추가

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-import {createApp} from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue' // package.json에 선언되어있는 라이브러리 안의 createApp메소드를 가져옴
+import App from './App.vue' // /App.vue에 선언되어있는 App을 import
 
-
-createApp(App).mount('#app')
+createApp(App).mount('#app') // import한 Vue앱을 public 폴더안에 index.html안의 id가 app인 Dom요소에 마운트
